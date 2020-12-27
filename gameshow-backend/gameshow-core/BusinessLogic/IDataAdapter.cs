b599853e65b0b4c9ad8ed18aced7072800c0593a
a-t-cole <8825764+a-t-cole@users.noreply.github.com>
2020-12-27 12:20:54 +0000

    Shuffled models to independent class library

---
+++ gameshow-backend/gameshow-core/BusinessLogic/IDataAdapter.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace gameshow_backend.BusinessLogic
+{
+    interface IDataAdapter
+    {
+    }
+}
